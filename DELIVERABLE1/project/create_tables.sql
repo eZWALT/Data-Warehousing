9a4e14f0ca4bdee2ebea38fc8f9bc6e6fc0033a2
kelokito <125611839+kelokito@users.noreply.github.com>
2024-10-23 17:04:49 +0200

    Add table creation first version

---
+++ DELIVERABLE1/project/create_tables.sql
@@ -0,0 +1,45 @@
+CREATE TABLE aircraft(
+    aircraftId CHAR(6),
+    model VARCHAR(4),
+    manufacturer VARCHAR(6)
+    PRIMARY KEY (aircraftId)
+);
+
+
+CREATE TABLE flight(
+    aircraftId CHAR(6),
+    duration INTERVAL,
+    cancelled BOOLEAN,
+    FOREIGN KEY aircraftId REFERENCES aircraft(aircraftId)
+);
+CREATE TYPE MaintenanceEventKind AS ENUM ('Delay', 'Safety', 'AircraftOnGround', 'Maintenance', 'Revision');
+
+CREATE TABLE maintenanceEvent(
+    aircraftId CHAR(6),
+    duration INTERVAL,
+    kind MaintenanceEventKind,
+    FOREIGN KEY aircraftId REFERENCES aircraft(aircraftId)  
+);
+
+CREATE TYPE ReportKind AS ENUM ('PIREP', 'MAREP'); 
+
+CREATE TABLE reporter(
+    reporterId SMALLINT,
+    airport CHAR(3)
+    PRIMARY KEY (reporterId)
+);
+
+CREATE TABLE technicalLogBook (
+    aircraftId CHAR(6),
+    reporterClass ReportKind,
+    reporterId SMALLINT,
+    FOREIGN KEY aircraftId REFERENCES aircraft(aircraftId)
+    FOREIGN KEY reporterId REFERENCES reporter(reporterId)
+);
+
+
+
+
+
+
+
