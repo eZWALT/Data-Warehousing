e19cbbda08a187fac3add129cd99e45b50f1d9e5
kelokito <adria.espinoza@estudiantat.upc.edu>
2024-11-02 19:58:34 +0100

    changed some queries

--- DELIVERABLE1/project/final_create_tables.sql
+++ DELIVERABLE1/project/final_create_tables.sql
@@ -1,57 +1,74 @@
 -- TABLE CREATION
 
-CREATE TABLE aircraft(
-    aircraftId CHAR(6),
-    model VARCHAR(4),
-    manufacturer VARCHAR(6),
+CREATE TABLE aircraft (
+    aircraftId CHAR(6) NOT NULL,
+    model VARCHAR2(4),
+    manufacturer VARCHAR2(6),
     PRIMARY KEY (aircraftId)
 );
 
+CREATE TABLE dia(
+	dia DATE,
+	mes INTEGER,
+	anyo INTEGER,
+	PRIMARY KEY(dia)
+);
 
-CREATE TABLE flightStats(
-    aircraftId CHAR(6),
+
+CREATE TABLE flightStats (
+    aircraftId CHAR(6) NOT NULL,
+    flightDay DATE NOT NULL,
     flight_hours INTEGER,
     departures INTEGER,
     cancellations INTEGER,
     delays INTEGER,
-    FOREIGN KEY aircraftId REFERENCES aircraft(aircraftId) NOT NULL,
-    PRIMARY KEY (aircraftId)
+    PRIMARY KEY (aircraftId, flightDay),
+    FOREIGN KEY (aircraftId) REFERENCES aircraft(aircraftId)
 );
-CREATE TABLE maintenanceStats(
-    aircraftId CHAR(6),
-    programmed BOOLEAN,
+
+CREATE TABLE maintenanceStats (
+    aircraftId CHAR(6) NOT NULL,
+    mainDay DATE NOT NULL,
+    programmed CHAR(1) CHECK (programmed IN ('Y', 'N')), -- Using CHAR(1) for Boolean-like values
     timeUnavailable INTEGER,
-    FOREIGN KEY aircraftId REFERENCES aircraft(aircraftId) NOT NULL,
-    PRIMARY KEY (aircraftId)
+    PRIMARY KEY (aircraftId, mainDay),
+    FOREIGN KEY (aircraftId) REFERENCES aircraft(aircraftId),
+    FOREIGN KEY (mainDay) REFERENCES dia(dia)
 );
 
-CREATE TYPE ReportKind AS ENUM ('PIREP', 'MAREP'); 
-
-CREATE TABLE reporter(
+-- Since Oracle does not support ENUM, use a CHECK constraint
+CREATE TABLE reporter (
     reporterId SMALLINT,
+    reporterClass VARCHAR2(5) CHECK (reporterClass IN ('PIREP', 'MAREP')),
     airport CHAR(3),
     PRIMARY KEY (reporterId)
 );
 
-CREATE TABLE technicalLogBook (
-    aircraftId CHAR(6),
-    reporterClass ReportKind,
-    reporterId SMALLINT,
+
+CREATE TABLE tlbStats (
+    aircraftId CHAR(6) NOT NULL,
+    tlbDay DATE NOT NULL,
+    reporterId SMALLINT NOT NULL,
     logBookCount INTEGER,
-    FOREIGN KEY reporterId REFERENCES reporter(reporterId),
-    FOREIGN KEY aircraftId REFERENCES aircraft(aircraftId) NOT NULL,
-    PRIMARY KEY (aircraftId)
+    PRIMARY KEY (aircraftId),
+    FOREIGN KEY (reporterId) REFERENCES reporter(reporterId),
+    FOREIGN KEY (aircraftId) REFERENCES aircraft(aircraftId),
+    FOREIGN KEY (tlbDay) REFERENCES dia(dia)
 );
 
+-- DROP TABLE STATEMENTS 
+DROP TABLE dia;
+DROP TABLE aircraft;
+DROP TABLE reporter;
+DROP TABLE maintenanceStats;
+DROP TABLE flightStats;
+DROP TABLE tlbStats;
 
---INDEX CREATION (if the creation of materialized views is so slow)
+
+
+-- INDEX CREATION
 
 CREATE INDEX air_indx ON aircraft(aircraftId);
-
-CREATE INDEX flight_indx ON flight(aircraftId);
-
-CREATE INDEX mant_indx ON maintenanceEvent(aircraftId);
-
+CREATE INDEX flight_indx ON flightStats(aircraftId);
+CREATE INDEX mant_indx ON maintenanceStats(aircraftId);
 CREATE INDEX tlb_indx ON technicalLogBook(aircraftId);
-
-
